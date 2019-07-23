22d8a9a157d4b1119ab73756b3d55d948a0615c9
Andrew Williams <1williamsaj@gmail.com>
2019-07-23 23:35:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,8 +38,8 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  #source_array.reduce {|x, y| x && y}
-  
+  source_array.reduce {|x, y| x && y}
+
 end
 
 def reduce_to_any_true (source_array)
