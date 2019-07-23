9480f91a6eec42fb42ef82d9fdec7b1259ed44d9
Andrew Williams <1williamsaj@gmail.com>
2019-07-23 23:50:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,10 +42,7 @@ def reduce_to_all_true(source_array)
   i = 0
 
   while i < source_array.length do
-    if source_array[i] == false
-      return false
-    else
-      return true
+    
   end
 end
 
